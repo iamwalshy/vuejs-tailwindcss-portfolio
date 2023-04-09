fe92ac28a24ba9c6291f7c32760e9350078eed32
iamwalshy <iamwalshy@protonmail.com>
2023-04-08 23:26:30 -0400

    changing top left logo for website & fixing errors

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -1,48 +1,44 @@
 <script>
 export default {
-	setup() {
-		return {
-			bios: [
-				{
-					id: 1,
-					bio:
-						'William Walsh, also known as Walshe, is a self-taught full-stack developer with a passion for video games. With his dedication to continuously learning and staying up-to-date with the latest technologies, he has developed a wide range of skills and expertise in web development, including front-end and back-end development, database management, and more. His strong problem-solving skills and attention to detail make him a valuable asset to any team he works with.
-
-Walshe's love for video games and technology began at a young age and has continued to inspire him throughout his career. He is constantly seeking new and innovative ways to incorporate his passion into his work and projects. In his free time, he enjoys playing video games and tinkering with new technologies to explore their potential applications.
-
-Overall, William Walsh, or Walshe, is a driven and talented developer with a unique perspective and a passion for innovation. His commitment to excellence and his love for video games make him an asset to any team or project he works on.',
-				},
-				{
-					id: 2,
-					bio:
-						'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-				},
-			],
-		};
-	},
+  setup() {
+    return {
+      bios: [
+        {
+          id: 1,
+          bio:
+            "William Walsh, also known as Walshe, is a self-taught full-stack developer with a passion for video games. With his dedication to continuously learning and staying up-to-date with the latest technologies, he has developed a wide range of skills and expertise in web development, including front-end and back-end development, database management, and more. His strong problem-solving skills and attention to detail make him a valuable asset to any team he works with.\n\nWalshe's love for video games and technology began at a young age and has continued to inspire him throughout his career. He is constantly seeking new and innovative ways to incorporate his passion into his work and projects. In his free time, he enjoys playing video games and tinkering with new technologies to explore their potential applications. Overall, William Walsh, or Walshe, is a driven and talented developer with a unique perspective and a passion for innovation. His commitment to excellence and his love for video games make him an asset to any team or project he works on.",
+        },
+        {
+          id: 2,
+          bio:
+            "I believe I am a talented developer with a unique perspective and a passion for innovation. His commitment to excellence and his love for video games make him an asset to any team or project he works on.",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <template>
-	<div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
-		<!-- About profile image -->
-		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
-			<img
-				src="@/assets/images/profile.jpeg"
-				class="rounded-xl w-96"
-				alt=""
-			/>
-		</div>
+  <div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+    <!-- About profile image -->
+    <div class="w-full sm:w-1/4 mb-7 sm:mb-0">
+      <img
+        src="@/assets/images/profile.jpeg"
+        class="rounded-xl w-96"
+        alt=""
+      />
+    </div>
 
-		<!-- About details -->
-		<div class="w-full sm:w-3/4 text-left">
-			<p
-				v-for="bio in bios"
-				:key="bio.id"
-				class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
-			>
-				{{ bio.bio }}
-			</p>
-		</div>
-	</div>
+    <!-- About details -->
+    <div class="w-full sm:w-3/4 text-left">
+      <p
+        v-for="bio in bios"
+        :key="bio.id"
+        class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+      >
+        {{ bio.bio }}
+      </p>
+    </div>
+  </div>
 </template>
