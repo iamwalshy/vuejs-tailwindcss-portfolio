8355f37e41c7d4aecaf5f0bae70c30f7c57cbd2f
iamwalshy <iamwalshy@protonmail.com>
2023-04-08 23:29:45 -0400

    changing top left logo for website & fixing errors

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -1,198 +1,30 @@
 <script>
-import feather from 'feather-icons';
-import ProjectHeader from '../components/projects/ProjectHeader.vue';
-import ProjectGallery from '../components/projects/ProjectGallery.vue';
-import ProjectInfo from '../components/projects/ProjectInfo.vue';
-import ProjectRelatedProjects from '../components/projects/ProjectRelatedProjects.vue';
-
-export default {
-	name: 'Projects',
-	components: {
-		ProjectHeader,
-		ProjectGallery,
-		ProjectInfo,
-		ProjectRelatedProjects,
-	},
-	data: () => {
-		return {
-			singleProjectHeader: {
-				singleProjectTitle: 'Project Management UI',
-				singleProjectDate: 'Jul 26, 2021',
-				singleProjectTag: 'UI / Frontend',
-			},
-			projectImages: [
-				{
-					id: 1,
-					title: 'Kabul Project Management UI',
-					img: require('@/assets/images/ui-project-1.jpg'),
-				},
-				{
-					id: 2,
-					title: 'Kabul Project Management UI',
-					img: require('@/assets/images/web-project-2.jpg'),
-				},
-				{
-					id: 3,
-					title: 'Kabul Project Management UI',
-					img: require('@/assets/images/mobile-project-2.jpg'),
-				},
-			],
-			projectInfo: {
-				clientHeading: 'About Client',
-				companyInfos: [
+	export default {
+		setup() {
+			return {
+				projects: [
 					{
 						id: 1,
-						title: 'Name',
-						details: 'Company Ltd',
+						title: 'Project Title 1',
+						image: '@/assets/images/project1.png',
+						details:
+							'William Walsh, also known as Walshe, is a self-taught full-stack developer with a passion for video games. With his dedication to continuously learning and staying up-to-date with the latest technologies, he has developed a wide range of skills and expertise in web development, including front-end and back-end development, database management, and more. His strong problem-solving skills and attention to detail make him a valuable asset to any team he works with.',
+						tools: ['Vue.js', 'Tailwind CSS'],
+						demoLink: 'https://example.com',
+						sourceLink: 'https://github.com/example',
 					},
 					{
 						id: 2,
-						title: 'Services',
-						details: 'UI Design & Frontend Development',
-					},
-					{
-						id: 3,
-						title: 'Website',
-						details: 'https://company.com',
-					},
-					{
-						id: 4,
-						title: 'Phone',
-						details: '555 8888 888',
-					},
-				],
-				objectivesHeading: 'Objective',
-				objectivesDetails:
-					'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, natus! Quibusdam enim quod in esse, mollitia molestias incidunt quas ipsa accusamus veniam.',
-				technologies: [
-					{
-						title: 'Tools & Technologies',
-						techs: [
-							'HTML',
-							'CSS',
-							'JavaScript',
-							'Vue.js',
-							'TailwindCSS',
-							'AdobeXD',
-						],
-					},
-				],
-				projectDetailsHeading: 'Challenge',
-				projectDetails: [
-					{
-						id: 1,
+						title: 'Project Title 2',
+						image: '@/assets/images/project2.png',
 						details:
-							'William Walsh, also known as Walshe, is a self-taught full-stack developer with a passion for video games. With his dedication to continuously learning and staying up-to-date with the latest technologies, he has developed a wide range of skills and expertise in web development, including front-end and back-end development, database management, and more. His strong problem-solving skills and attention to detail make him a valuable asset to any team he works with.
-
-Walshe's love for video games and technology began at a young age and has continued to inspire him throughout his career. He is constantly seeking new and innovative ways to incorporate his passion into his work and projects. In his free time, he enjoys playing video games and tinkering with new technologies to explore their potential applications.
-
-Overall, William Walsh, or Walshe, is a driven and talented developer with a unique perspective and a passion for innovation. His commitment to excellence and his love for video games make him an asset to any team or project he works on.',
-					},
-					{
-						id: 2,
-						details:
-							'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-					},
-					{
-						id: 3,
-						details:
-							'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
-					},
-					{
-						id: 4,
-						details:
-							'William Walsh, also known as Walshe, is a self-taught full-stack developer with a passion for video games. With his dedication to continuously learning and staying up-to-date with the latest technologies, he has developed a wide range of skills and expertise in web development, including front-end and back-end development, database management, and more. His strong problem-solving skills and attention to detail make him a valuable asset to any team he works with.
-
-Walshe's love for video games and technology began at a young age and has continued to inspire him throughout his career. He is constantly seeking new and innovative ways to incorporate his passion into his work and projects. In his free time, he enjoys playing video games and tinkering with new technologies to explore their potential applications.
-
-Overall, William Walsh, or Walshe, is a driven and talented developer with a unique perspective and a passion for innovation. His commitment to excellence and his love for video games make him an asset to any team or project he works on.',
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sagittis mauris ac velit blandit, nec suscipit nibh commodo. Donec vitae neque ut mauris vehicula lacinia. Sed nec rhoncus urna, at iaculis arcu. Sed euismod mauris euismod dui mattis dapibus.',
+						tools: ['React', 'CSS'],
+						demoLink: 'https://example.com',
+						sourceLink: 'https://github.com/example',
 					},
 				],
-				socialSharingsHeading: 'Share This',
-				socialSharings: [
-					{
-						id: 1,
-						name: 'Twitter',
-						icon: 'twitter',
-						url: 'https://twitter.com/realstoman',
-					},
-					{
-						id: 2,
-						name: 'Instagram',
-						icon: 'instagram',
-						url: 'https://instagram.com/realstoman',
-					},
-					{
-						id: 3,
-						name: 'Facebook',
-						icon: 'facebook',
-						url: 'https://facebook.com/',
-					},
-					{
-						id: 4,
-						name: 'LinkedIn',
-						icon: 'linkedin',
-						url: 'https://linkedin.com/',
-					},
-					{
-						id: 5,
-						name: 'Youtube',
-						icon: 'youtube',
-						url: 'https://www.youtube.com/c/StomanStudio',
-					},
-				],
-			},
-			relatedProject: {
-				relatedProjectsHeading: 'Related Projects',
-				relatedProjects: [
-					{
-						id: 1,
-						title: 'Mobile UI',
-						img: require('@/assets/images/mobile-project-1.jpg'),
-					},
-					{
-						id: 2,
-						title: 'Web Application',
-						img: require('@/assets/images/web-project-1.jpg'),
-					},
-					{
-						id: 3,
-						title: 'UI Design',
-						img: require('@/assets/images/ui-project-2.jpg'),
-					},
-					{
-						id: 4,
-						title: 'Kabul Mobile App UI',
-						img: require('@/assets/images/mobile-project-2.jpg'),
-					},
-				],
-			},
-		};
-	},
-	mounted() {
-		feather.replace();
-	},
-	updated() {
-		feather.replace();
-	},
-	methods: {},
-};
+			};
+		},
+	};
 </script>
-
-<template>
-	<div class="container mx-auto mt-10 sm:mt-20">
-		<!-- Project header -->
-		<ProjectHeader :singleProjectHeader="singleProjectHeader" />
-
-		<!-- Project gallery -->
-		<ProjectGallery :projectImages="projectImages" />
-
-		<!-- Project information -->
-		<ProjectInfo :projectInfo="projectInfo" />
-
-		<!-- Project related projects -->
-		<ProjectRelatedProjects :relatedProject="relatedProject" />
-	</div>
-</template>
-
-<style scoped></style>
